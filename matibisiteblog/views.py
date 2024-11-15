934f69a3654712a85ad816f574518448e8a676b8
Kudakwashe Marongedza <107895856+Ashemaro@users.noreply.github.com>
2024-11-15 15:16:05 +0200

    Removed Mixins import

--- matibisiteblog/views.py
+++ matibisiteblog/views.py
@@ -4,7 +4,7 @@ from .forms import ContactForm, NewsForm, RegistrationForm
 from .models import News, VisitCounter, NookHubActivities
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixincd
+
 
 
 class NewsList(generic.ListView):
